55fa29ddcb7de7ac496a0b071d45f4c720161bb4
上野佑樹 <>
2019-11-14 10:22:48 +0800

    11月14日
    授業内更新

--- app/Http/Requests/CreateDiary.php
+++ app/Http/Requests/CreateDiary.php
@@ -31,4 +31,12 @@ class CreateDiary extends FormRequest
 
         ];
     }
+    //エラー文言を表示する際の、name属性と表示名の設定をする
+    public function attributes()
+    {
+        return [
+            'title' => 'タイトル',
+            'body'  => '本文'
+        ];
+    }
 }
